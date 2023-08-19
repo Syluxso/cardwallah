94a7faaafb3d9533b56b23e3c4a902e60a98d9e1
Darryn Nyberg <darryn@nybergtechnology.com>
2023-08-18 18:44:29 -0700

    form for new card

--- www/cardwallah/src/NewCardForm.js
+++ www/cardwallah/src/NewCardForm.js
@@ -1,17 +1,24 @@
 import React from "react";
+import uuidv4 from "uuid/v4";
 
 export default function NewCardForm() {
+  const cardNameRef = React.useRef(null);
 
   function handleCardNew(e) {
-
+    const name = cardNameRef.current.value;
+    if (name === '') return;
+    
+    cardNameRef.current.value = null;
   }
+
   return (
     <>
       <input
         type="text"
         placeholder="Card title..."
+        ref={cardNameRef} 
       />
       <button onClick={handleCardNew} >Add Card</button>
     </>
   );
-}
+} 
